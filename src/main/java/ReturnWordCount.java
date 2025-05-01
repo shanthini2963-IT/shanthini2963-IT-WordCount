d70a1c1bbfc6cf1becf2da0f2053e50d8947a717
shanthini2963-IT <shanthiniramanjaneya@gmail.com>
2025-05-01 07:52:51 +0530

    coding challenge word count

--- src/main/java/ReturnWordCount.java
+++ src/main/java/ReturnWordCount.java
@@ -5,6 +5,7 @@ public class ReturnWordCount {
      * @return return the amount of words in a string.
      */
     public int count(String in){
-        return 0;
+        String[] words = in.trim().split("\\s+");
+        return words.length;
     }
 }
